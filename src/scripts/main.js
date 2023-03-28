14a26edaa91bbf6b578ceac21c33cf4bbb83d81e
Alex <kartanovaleks@gmail.com>
2023-03-28 22:05:38 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
-'use strict';
+"use strict";
 
-// write code here
+const headers = document.querySelectorAll("span");
+
+let isClicked = false;
+
+for (let header of headers) {
+  header.addEventListener("click", () => {
+    isClicked = !isClicked;
+    if (isClicked) {
+      header.nextElementSibling.style.display = "none";
+    } else {
+      header.nextElementSibling.style.display = "";
+    }
+  });
+}
